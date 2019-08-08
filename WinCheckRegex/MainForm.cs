0487bb8c7c364e259732094c454551c1640b85a4
kazenetu <rakudafactory@gmail.com>
2019-08-09 00:58:53 +0900

    正規表現処理を実装

--- WinCheckRegex/MainForm.cs
+++ WinCheckRegex/MainForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace WinCheckRegex
@@ -15,6 +9,41 @@ namespace WinCheckRegex
     public MainForm()
     {
       InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // 結果をクリア
+      ResultText.Text = "";
+
+      // 入力情報取得
+      var src = SrcText.Text;
+      var reg = RegexText.Text;
+      var replace = ReplaceText.Text;
+      var result = "";
+
+      // 対象文字列から正規表現で該当するか確認
+      var matches = Regex.Matches(src, reg);
+      if (matches.Count < 1 || matches[0].Groups.Count <= 1)
+      {
+        return;
+      }
+
+      // 正規表現で取得できた要素の置き換え
+      foreach (Match match in matches)
+      {
+        var matcheGroup = match.Groups;
+        var replaceSentence = replace;
+        for (var i = 1; i < matcheGroup.Count; i++)
+        {
+          replaceSentence = replaceSentence.Replace($"${ i}", matcheGroup[i].Value);
+          src = src.Replace(matcheGroup[i].Value, replaceSentence);
+        }
+        result += replaceSentence + Environment.NewLine;
+      }
+
+      // 置き換え結果を表示
+      ResultText.Text = $"置き換え後の対象文字:{Environment.NewLine}{result}{Environment.NewLine}置換後:{Environment.NewLine}{src}";
     }
   }
 }
